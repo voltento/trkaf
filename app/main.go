ae42e0ac7658503388307660409ce779d5dca6e8
v.dumanovskii <v.dumanovskii@corp.mail.ru>
2021-01-22 15:50:43 +0300

    Add cobra.

--- app/main.go
+++ app/main.go
@@ -1,55 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"flag"
-	"github.com/voltento/trkaf/internal/kf"
-	"log"
-	"os"
-	"strconv"
-	"time"
-)
-
-const addr = "127.0.0.1:9092"
-const topic = "count_2_partitions"
-
-func isWriterApp() bool {
-	isWriter := flag.Bool("w", false, "is writer app")
-	flag.Parse()
-	return *isWriter
-}
-
-func getRoundIterator(from, to int) func() int {
-	if from > to {
-		from, to = to, from
-	}
-
-	counter := 0
-	return func() int {
-		r := counter
-		counter += 1
-		if counter >= to {
-			counter = from
-		}
-		return r
-	}
-}
+import "github.com/voltento/trkaf/cmd"
 
 func main() {
-	const tcp = "tcp"
-	conn := kf.NewKafka(tcp, addr)
-
-	if isWriterApp() {
-		log.Printf("start writing to the topic: %s\n", topic)
-		i := 0
-		partition := getRoundIterator(0, 2)
-		for {
-			conn.WriteToKafka(topic, partition(), []byte(strconv.Itoa(i)))
-			<-time.After(time.Second * 2)
-			i += 1
-		}
-	} else {
-		log.Printf("start read from the topic: %s\n", topic)
-		const flags = log.Lmsgprefix | log.Ldate | log.Lmicroseconds
-		conn.ReadFromKafka(topic, log.New(os.Stdout, "worker_1: ", flags))
-	}
+	cmd.Execute()
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/confluentinc/confluent-kafka-go v1.5.2 // indirect
 	github.com/segmentio/kafka-go v0.4.8
+	github.com/spf13/cobra v1.1.1 // indirect
 	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.5.2 // indirect
 )
